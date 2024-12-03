dc27d79f8a4c9c1c6b0c32ba0fdf76387c0cfed3
VixonyAn <vixonyan@gmail.com>
2024-12-03 10:11:47 +0100

    Added interfaces, models, services

---
+++ ClassLibrary/Models/Boat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Interfaces;
+
+namespace ClassLibrary.Models
+{
+    public class Boat : IBoat
+    {
+        // boat (grasp) is the expert on it's own issues and maintenance
+        // boat includes "maintenance log" of issues
+        // udover default + en ny constructor til salg opslag (ejer's kontakt/pris i Post)
+    }
+}
---
+++ ClassLibrary/Services/CourseRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Interfaces;
+
+namespace ClassLibrary.Services
+{
+    public class CourseRepository : ICourseRepository
+    {
+    }
+}
