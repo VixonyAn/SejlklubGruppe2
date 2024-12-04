2000cfbd7e13d83e314ef77bbae8e8b063a9a25c
VixonyAn <vixonyan@gmail.com>
2024-12-04 11:11:52 +0100

    fixed error in program after changing constructors in model and boat

--- SejlklubGruppe2/Program.cs
+++ SejlklubGruppe2/Program.cs
@@ -5,8 +5,8 @@ internal class Program
     private static void Main(string[] args)
     {
         #region VTest
-        Model optimistjolle = new Model("Optimistjolle", 00.00, 00.00, 00.00, 00.00);
-        Boat b1 = new Boat(optimistjolle, "hyggelig for begyndere", "5678");
+        Model optimistjolle = new Model("Optimistjolle", "hyggelig for begyndere", 00.00, 00.00, 00.00, 00.00);
+        Boat b1 = new Boat(optimistjolle, "5678");
         Console.WriteLine(b1);
         //Kommentar
         #endregion
