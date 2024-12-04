ae1582f13d46fa9c97b796f5d181c4743b42f238
Krelle156 <kotofteng@gmail.com>
2024-12-04 14:24:22 +0100

    Reimplmented Mockdata as a singleton.
    Fixed an error where the program would claim there was no project reference. Presumably due to a previous "fix" of mine. even if nothing further breaks this should probably be discussed briefly.

--- ClassLibrary/Data/MockData.cs
+++ ClassLibrary/Data/MockData.cs
@@ -9,39 +9,63 @@ namespace ClassLibrary.Data
 {
     public class MockData
     {
-        //#region Instance fields
-        //Model optimistjolle = new Model("Optimistjolle", "hyggelig for begyndere", 00.00, 00.00, 00.00, 00.00);
-        //Model laserjolle = new Model("Laserjolle", "enkel og let for alle aldersgrupper", 00.00, 00.00, 00.00, 00.00);
+        #region Instance fields
+        private static MockData _theInstance;
 
-        //Boat b1 = new Boat(optimistjolle, "1234");
+        private Model optimistjolle;
+        private Model laserjolle;
 
-        //private Dictionary<string, Model> _modelData =
-        //    new Dictionary<string, Model>()
-        //    {
-        //    { "Optimistjolle", new Model("Optimistjolle", "hyggelig for begyndere", 00.00, 00.00, 00.00, 00.00) },
-        //    { "Laserjolle", new Model("Laserjolle", "enkel og let for alle aldersgrupper", 00.00, 00.00, 00.00, 00.00) },
-        //    };
 
-        //private Dictionary<string, Boat> _boatData =
-        //    new Dictionary<string, Boat>()
-        //    {
-        //    { "1234", new Boat(optimistjolle, "1234") },
-        //    { "5678", new Boat(optimistjolle, "5678") },
-        //    { "9012", new Boat(optimistjolle, "9012") },
-        //    { "3456", new Boat(laserjolle, "3456") }
-        //    };
-        //#endregion
+        private Dictionary<string, Model> _modelData;
 
-        //#region Properties
-        //public Dictionary<string, Model> ModelData
-        //{
-        //    get { return _modelData; }
-        //}
+        private Dictionary<string, Boat> _boatData;
+        #endregion
 
-        //public Dictionary<string, Boat> BoatData
-        //{
-        //    get { return _boatData; }
-        //}
-        //#endregion
+        #region Constructors
+        private MockData()
+        {
+            optimistjolle = new Model("Optimistjolle", "hyggelig for begyndere", 00.00, 00.00, 00.00, 00.00);
+            laserjolle = new Model("Laserjolle", "enkel og let for alle aldersgrupper", 00.00, 00.00, 00.00, 00.00);
+
+            _modelData = new Dictionary<string, Model>()
+            {
+            { "Optimistjolle", optimistjolle },
+            { "Laserjolle", laserjolle },
+            };
+
+            _boatData = new Dictionary<string, Boat>()
+            {
+            { "1234", new Boat(optimistjolle, "1234") },
+            { "5678", new Boat(optimistjolle, "5678") },
+            { "9012", new Boat(optimistjolle, "9012") },
+            { "3456", new Boat(laserjolle, "3456") }
+            };
+        }
+        #endregion
+
+        #region Properties
+        public Dictionary<string, Model> ModelData
+        {
+            get { return _modelData; }
+        }
+
+        public Dictionary<string, Boat> BoatData
+        {
+            get { return _boatData; }
+        }
+        #endregion
+
+        #region Methods
+
+        public static MockData GetInstance()
+        {
+            if(_theInstance == null)
+            {
+                _theInstance = new MockData();
+            }
+            return _theInstance;
+        }
+
+        #endregion
     }
 }
