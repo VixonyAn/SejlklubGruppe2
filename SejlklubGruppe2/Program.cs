c6ed468b10d7a996bde9bf267466d2116e09155e
Krelle156 <kotofteng@gmail.com>
2024-12-04 11:10:08 +0100

    Added test-regions to program

--- SejlklubGruppe2/Program.cs
+++ SejlklubGruppe2/Program.cs
@@ -4,9 +4,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        #region VTest
         Model optimistjolle = new Model("Optimistjolle", 00.00, 00.00, 00.00, 00.00);
         Boat b1 = new Boat(optimistjolle, "hyggelig for begyndere", "5678");
         Console.WriteLine(b1);
         //Kommentar
+        #endregion
+
+        #region Atest
+
+        #endregion
+
+        #region KTest
+
+        #endregion
+
+        #region LTest
+
+        #endregion
     }
 }
